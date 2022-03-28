3a9606b4ff4cd8684c5af87edf2a3ce8637a4908
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-28 15:45:43 -0700

    added a notes.js and more work on the server.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,26 @@
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const notes = require("express").Router();
+
+notes.get("/", (req, res) => {
+  console.info(`${req.method} request received for the notes`);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post("/", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { activenote } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      activenote,
+      tip_id: uuid(),
+    };
+
+    readAndAppend(newTip, "./db/tips.json");
+  }
+});
+
+module.exports = notes;
