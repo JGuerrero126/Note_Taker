921fa9af4546f0712980e6e809090b0f10a91bc2
Jerry Guerrero <jerry1996714@gmail.com>
2022-03-28 16:56:17 -0700

    more work on the back-end

--- routes/notes.js
+++ routes/notes.js
@@ -4,22 +4,22 @@ const notes = require("express").Router();
 
 notes.get("/", (req, res) => {
   console.info(`${req.method} request received for the notes`);
-  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  fs.readFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 notes.post("/", (req, res) => {
   console.info(`${req.method} request received to add a note`);
   console.log(req.body);
 
-  const { activenote } = req.body;
+  const { body } = req.body;
 
   if (req.body) {
-    const newTip = {
-      activenote,
-      tip_id: uuid(),
+    const newNote = {
+      body,
+      tip_id: uuidv4(),
     };
 
-    readAndAppend(newTip, "./db/tips.json");
+    fs.readFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
   }
 });
 
